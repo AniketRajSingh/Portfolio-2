2615f9ca2a3757beb05e89a56559b7fee8daf2fd
Aniket Raj Singh <mohitrajingh223@gmail.com>
2022-09-12 21:57:20 +0530

    Finished Basic Pages - Advanced Editing Remains

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,28 +1,54 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("page-include-html");
-      if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Some Issue With the Required Page Plese Reload The Webpage";}
-            /*remove the attribute, and call this function once more:*/
-            elmnt.removeAttribute("page-include-html");
-            includeHTML();
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("page-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
           }
-        }      
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /*exit the function:*/
-        return;
-      }
+          if (this.status == 404) {
+            elmnt.innerHTML =
+              "Some Issue With the Required Page Plese Reload The Webpage";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("page-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
     }
-  };
-  
+  }
+}
+
+// animations on scroll
+
+function reveal() {
+  var reveals = document.querySelectorAll(
+    ".text-focus-in,.typing-demo,.slide-top"
+  );
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
